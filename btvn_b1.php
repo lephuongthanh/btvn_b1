ae1622f237dd8b46e89f4257b204947d3059f557
lephuongthanh <phuongthanhle1505@gmail.com>
2024-08-21 17:22:47 +0700

    first

---
+++ btvn_b1.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>BTVN_Buoi1</title>
+   <style>
+      body {
+         display: flex;
+         flex-direction: column;
+         align-items: center;
+      }
+      h1 {
+         color: rgb(231, 93, 139);
+         font-size: 50px;
+         text-align: center;
+      }
+      p {   
+         font-size: 20px;
+         max-width: 50%;
+         margin-left: 20px;
+         text-align: justify;
+      }
+      .content {
+         display: flex;      
+      }
+      img {
+         width: 40%;
+         margin-left: 50px;
+      }
+   </style>
+</head>
+<body>
+<?php
+echo '<h1> BlackPink làm nên lịch sử khi trở thành nhóm nhạc nữ Kpop đầu tiên dẫn đầu bảng xếp hạng Billboard 200 với “Born Pink” </h1>';
+echo '<div class="content">';
+echo '<p> Ngày 25/9, Billboard thông báo album mới phát hành của BlackPink Born Pink đứng vị trí <b style="color:red;"> số 1 </b>trên bảng xếp hạng Top 200 Album của Billboard với 102.000 đơn vị album tương đương tại Mỹ tính tới 22/9.
+Thành tích ấn tượng này ghi tên BlackPink vào lịch sử khi trở thành nhóm nhạc nữ Kpop đầu tiên dẫn đầu Billboard 200. Các cô gái nhà YG cũng là nhóm nhạc nữ đầu tiên đạt vị trí quán quân của bảng xếp hạng âm nhạc uy tín hàng đầu thế giới này kể từ năm 2008, sau nhóm nhạc Mỹ Danity Kane với Welcome to the Dollhouse. 
+BlackPink cũng là nhóm nhạc Kpop thứ ba đứng đầu Billboard 200 trong năm 2022, sau BTS và Stray Kids.
+<br> <br>
+Theo Billboard, Born Pink cũng đứng vị trí <b style="color:red;"> số 1 </b> trên bảng xếp hạng Top Album Sales với 75.500 bản được bán ra trong tuần, xếp thứ 7 trong danh sách album bán chạy trong năm 2022. Trong tổng số đó, 85% là doanh số bán đĩa CD (64.000) và 15% là doanh số bán album kỹ thuật số (11.500).
+Born Pink được phát hành ngày 16/9, là album thứ hai của BlackPink. Album đầu tiên The Album cũng giữ thứ hạng cao khi mới trình làng, Top 2 của Billboard 200 vào tháng 10/2020.
+Trước Born Pink, đĩa đơn Pink Venom đạt vị trí thứ 22 trên bảng xếp hạng Billboard Hot 100 vào ngày 3/9.
+ </p>';
+?>
+<img src="https://imagev3.vietnamplus.vn/w1000/Uploaded/2024/bokttj/2022_08_19/Pink_Venom.jpg.webp" alt="BlackPink">
+<?php
+echo '</div>'; 
+?>
+</body>
+</html>
